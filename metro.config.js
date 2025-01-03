d83004e2e7deee4d13f38d5b6b45e04d952d17ad
Florian Muller <fmuller@student.42.fr>
2025-01-03 19:47:04 +0100

    feat: install gesture handler and reanimated

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
+
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('sql');
-module.exports = config;
+
+// For Drizzle (https://orm.drizzle.team/docs/connect-expo-sqlite#update-config-files)
+config.resolver.sourceExts.push("sql");
+
+// For react-reanimated (https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started/#step-3-wrap-metro-config-with-reanimated-wrapper-recommended)
+module.exports = wrapWithReanimatedMetroConfig(config);
