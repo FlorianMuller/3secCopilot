fcda67f1bbf3ea13c2f626a93f62aeeccec24934
Florian Muller <fmuller@student.42.fr>
2025-12-31 13:55:12 +0100

    wrap camera roll in CameraRollPage (to add a year selector in it)

--- src/features/CameraRoll/CameraRoll.tsx
+++ src/features/CameraRoll/CameraRoll.tsx
@@ -1,4 +1,3 @@
-import { LinearGradient } from "expo-linear-gradient";
 import * as MediaLibrary from "expo-media-library";
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { FlatList, View, ViewToken } from "react-native";
@@ -107,33 +106,24 @@ export default function CameraRoll({
   }
 
   return (
-    <>
-      {/* Linear gradient between thumbnails and phone status bar (time, wifi icon...) */}
-      {/* Todo: compute dynamically status bar size */}
-      <LinearGradient
-        colors={["rgba(0, 0, 0, 0.6)", "rgba(0, 0, 0, 0)"]}
-        style={{ width: "100%", height: 80, position: "absolute", zIndex: 100 }}
-      />
-
-      <FlatList
-        data={days.map((day) => ({
-          day,
-          videosOfTheDay: videosByDay[day.toDateString()] || [],
-          isVisible: visibleDays.has(day.toDateString()),
-        }))}
-        renderItem={(props) => <DaySection {...props} onMetadataUpdate={handleSingleMetadataUpdate} />}
-        keyExtractor={({ day }) => day.toDateString()}
-        indicatorStyle="white"
-        onEndReached={allVideoLoaded ? undefined : handleLoadNextBatch}
-        onEndReachedThreshold={300}
-        initialNumToRender={20}
-        maxToRenderPerBatch={20}
-        onViewableItemsChanged={onViewableItemsChanged}
-        viewabilityConfig={viewabilityConfig}
-        // To not render fist section under iPhone notch
-        // todo: use safe zone component to detect if there's a notch
-        ListHeaderComponent={<View style={{ height: 70 }} />}
-      />
-    </>
+    <FlatList
+      data={days.map((day) => ({
+        day,
+        videosOfTheDay: videosByDay[day.toDateString()] || [],
+        isVisible: visibleDays.has(day.toDateString()),
+      }))}
+      renderItem={(props) => <DaySection {...props} onMetadataUpdate={handleSingleMetadataUpdate} />}
+      keyExtractor={({ day }) => day.toDateString()}
+      indicatorStyle="white"
+      onEndReached={allVideoLoaded ? undefined : handleLoadNextBatch}
+      onEndReachedThreshold={300}
+      initialNumToRender={20}
+      maxToRenderPerBatch={20}
+      onViewableItemsChanged={onViewableItemsChanged}
+      viewabilityConfig={viewabilityConfig}
+      // To not render fist section under iPhone notch
+      // todo: use safe zone component to detect if there's a notch
+      ListHeaderComponent={<View style={{ height: 70 }} />}
+    />
   );
 }
---
+++ src/features/CameraRoll/CameraRollPage.tsx
@@ -0,0 +1,21 @@
+import { LinearGradient } from "expo-linear-gradient";
+import CameraRoll from "./CameraRoll";
+
+export function CameraRollPage() {
+  const startDate = new Date(); // Today at 23:59:59
+  startDate.setHours(23, 59, 59, 999);
+  const endDate = new Date(new Date().getFullYear(), 0, 1); // January 1st of current year
+
+  return (
+    <>
+      {/* Linear gradient between thumbnails and phone status bar (time, wifi icon...) */}
+      {/* Todo: compute dynamically status bar size */}
+      <LinearGradient
+        colors={["rgba(0, 0, 0, 0.6)", "rgba(0, 0, 0, 0)"]}
+        style={{ width: "100%", height: 80, position: "absolute", zIndex: 100 }}
+      />
+
+      <CameraRoll startDate={startDate} endDate={endDate} />
+    </>
+  );
+}
