19adba218032fd69e24f78f0b207c7a34b7611ac
Florian Muller <fmuller@student.42.fr>
2026-01-01 15:01:27 +0100

    compute real years periods depending on preferences

--- src/features/CameraRoll/CameraRollPage.tsx
+++ src/features/CameraRoll/CameraRollPage.tsx
@@ -5,10 +5,6 @@ import { View } from "react-native";
 import { usePeriod } from "./hooks/usePeriod";
 
 export function CameraRollPage() {
-  const startDate = new Date(); // Today at 23:59:59
-  startDate.setHours(23, 59, 59, 999);
-  const endDate = new Date(new Date().getFullYear(), 0, 1); // January 1st of current year
-
   const { periods, selectedPeriod, setSelectedPeriodId } = usePeriod();
 
   return (
@@ -20,11 +16,15 @@ export function CameraRollPage() {
         style={{ width: "100%", height: 80, position: "absolute", zIndex: 100 }}
       />
 
-      <PeriodSelector periods={periods} selectedPeriod={selectedPeriod} onSelectPeriod={setSelectedPeriodId} />
-      {/* Offset CameraRoll start to not appear under PeriodSelector: */}
-      <View style={{ height: 25 }} />
+      {periods && selectedPeriod && (
+        <>
+          <PeriodSelector periods={periods} selectedPeriod={selectedPeriod} onSelectPeriod={setSelectedPeriodId} />
+          {/* Offset CameraRoll start to not appear under PeriodSelector: */}
+          <View style={{ height: 25 }} />
 
-      <CameraRoll startDate={selectedPeriod.startDate} endDate={selectedPeriod.endDate} key={selectedPeriod.id} />
+          <CameraRoll startDate={selectedPeriod.startDate} endDate={selectedPeriod.endDate} key={selectedPeriod.id} />
+        </>
+      )}
     </>
   );
 }
--- src/features/CameraRoll/hooks/usePeriod.ts
+++ src/features/CameraRoll/hooks/usePeriod.ts
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import preferences from "../../../services/preferences";
+import { YearGroupingMode } from "../../Options/sections/YearGrouping";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export type Period = {
   id: string;
@@ -8,31 +11,95 @@ export type Period = {
 };
 
 export function usePeriod() {
-  // Hardcoded 3 calendar years for UI development
-  // TODO: Later compute dynamically based on yearGroupingMode and birthdayDate preferences
-  const periods: Period[] = [
-    {
-      id: "2025",
-      label: "2025",
-      startDate: new Date(2025, 11, 31, 23, 59, 59, 999),
-      endDate: new Date(2025, 0, 1),
-    },
-    {
-      id: "2024",
-      label: "2024",
-      startDate: new Date(2024, 11, 31, 23, 59, 59, 999),
-      endDate: new Date(2024, 0, 1),
-    },
-    {
-      id: "2023",
-      label: "2023",
-      startDate: new Date(2023, 11, 31, 23, 59, 59, 999),
-      endDate: new Date(2023, 0, 1),
-    },
-  ];
+  const { yearGroupingMode } = preferences.useYearGroupingModePreference();
+  const { birthdayDate } = preferences.useBirthdayDatePreference();
 
-  const [selectedPeriodId, setSelectedPeriodId] = useState(periods[1].id);
-  const selectedPeriod = periods.find((p) => p.id === selectedPeriodId)!;
+  const periods: Period[] | undefined = computePeriods(yearGroupingMode, birthdayDate);
+
+  const [selectedPeriodId, setSelectedPeriodId] = useState<string>();
+  const selectedPeriod = periods?.find((p) => p.id === selectedPeriodId);
+
+  useEffect(() => {
+    // If no period selected, select the first one
+    if (periods !== undefined && selectedPeriodId === undefined && periods.length > 0) {
+      setSelectedPeriodId(periods[0].id);
+    }
+  }, [periods, selectedPeriodId]);
 
   return { periods, selectedPeriod, setSelectedPeriodId };
+}
+
+function computePeriods(yearGroupingMode: YearGroupingMode | undefined, birthdayDate?: Date | null | undefined) {
+  if (yearGroupingMode === undefined) {
+    return undefined;
+  }
+  if (yearGroupingMode === "age") {
+    if (birthdayDate === undefined || birthdayDate === null) {
+      // Todo: show error ?
+      return undefined;
+    }
+    return computeAgeYearPeriods(birthdayDate);
+  }
+
+  return computeCalendarYearPeriods();
+}
+
+function computeCalendarYearPeriods(lastYear = 1990): Period[] {
+  const currentYear = new Date().getFullYear();
+  const periods: Period[] = [];
+
+  for (const i of Array(currentYear - lastYear + 1).keys()) {
+    const year = currentYear - i;
+    periods.push({
+      id: `calendar-${year}`,
+      label: year.toString(),
+      startDate: new Date(year, 11, 31, 23, 59, 59, 999),
+      endDate: new Date(currentYear - i, 0, 1),
+    });
+  }
+
+  return periods;
+}
+
+function computeAgeYearPeriods(birthdayDate: Date): Period[] {
+  const currentAge = getAge(birthdayDate);
+  const periods: Period[] = [];
+
+  for (const i of Array(currentAge + 1).keys()) {
+    const age = currentAge - i;
+
+    // End camera roll on birthday
+    const endDate = new Date(birthdayDate);
+    endDate.setFullYear(birthdayDate.getFullYear() + age);
+    endDate.setHours(0, 0, 0, 0);
+
+    // Start camera roll on day before next birthday
+    const startDate = new Date(birthdayDate);
+    startDate.setFullYear(birthdayDate.getFullYear() + age + 1);
+    startDate.setDate(startDate.getDate() - 1);
+    startDate.setHours(23, 0, 0, 0);
+
+    periods.push({
+      id: `age-${age}`,
+      label: age >= 2 ? `${age} ans` : `${age} an`,
+      startDate,
+      endDate,
+    });
+  }
+
+  return periods;
+}
+
+function getAge(birthDate: Date, now: Date = new Date()): number {
+  let age = now.getFullYear() - birthDate.getFullYear();
+
+  const hasHadBirthdayThisYear =
+    now.getMonth() > birthDate.getMonth() ||
+    (now.getMonth() === birthDate.getMonth() && now.getDate() >= birthDate.getDate());
+
+  if (!hasHadBirthdayThisYear) {
+    age -= 1;
+  }
+
+  return age;
 }
